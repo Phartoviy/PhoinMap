bef86fd37f6b142adb545a28cf2fe1e7dc78b2d5
phart <uran0215@mail.ru>
2025-04-16 20:42:00 +0300

    Рефакторинг методов класса

--- phoinlist.cpp
+++ phoinlist.cpp
@@ -2,28 +2,23 @@
 
 
 template<typename T>
-PhoinList<T>::PhoinList(const std::unordered_map<T, char> &map)
-{
+PhoinList<T>::PhoinList(const std::unordered_map<T, char> &map){
     mapa = map;
 }
 
 template<typename T>
-void PhoinList<T>::appendValue(const T &value)
-{
+void PhoinList<T>::appendValue(const T &value){
     mapa[value] = ' ';
 }
 
 template<typename T>
-void PhoinList<T>::removeValue(const T &value)
-{
+void PhoinList<T>::removeValue(const T &value){
     mapa.erase(value);
 }
 
 template<typename T>
-bool PhoinList<T>::getValue(const int &number, T &value) const
-{
-    if (number < mapa.size())
-    {
+bool PhoinList<T>::getValue(const int &number, T &value) const{
+    if (number < mapa.size()){
         value = mapa.begin()+number;
         return true;
     }
@@ -31,49 +26,41 @@ bool PhoinList<T>::getValue(const int &number, T &value) const
 }
 
 template<typename T>
-bool PhoinList<T>::findValue(const T &value) const
-{
+bool PhoinList<T>::findValue(const T &value) const{
     return mapa.find(value) != mapa.end();
 }
 
 template<typename T>
-void PhoinList<T>::clear()
-{
+void PhoinList<T>::clear(){
     mapa.clear();
 }
 
 template<typename T>
-bool PhoinList<T>::isEmpty() const
-{
+bool PhoinList<T>::isEmpty() const{
     return mapa.empty();
 }
 
 template<typename T>
-size_t PhoinList<T>::size() const
-{
+size_t PhoinList<T>::size() const{
     return mapa.size();
 }
 
 template<typename T>
-bool PhoinList<T>::operator==(const PhoinList &other) const
-{
+bool PhoinList<T>::operator==(const PhoinList &other) const{
     return mapa == other.mapa;
 }
 
 template<typename T>
-bool PhoinList<T>::operator>(const PhoinList &other) const
-{
+bool PhoinList<T>::operator>(const PhoinList &other) const{
     return mapa.size() > other.mapa.size();
 }
 
 template<typename T>
-bool PhoinList<T>::operator<(const PhoinList &other) const
-{
+bool PhoinList<T>::operator<(const PhoinList &other) const{
     return mapa.size() < other.mapa.size();
 }
 
 template<typename T>
-void PhoinList<T>::operator =(const PhoinList &other) const
-{
+void PhoinList<T>::operator =(const PhoinList &other) const{
     mapa = other.mapa;
 }
